291ce8bac33968e9408f3ee3fd231a77c7905d32
masahiro96848 <feetwcpic9436@gmail.com>
2024-07-21 11:47:37 +0900

    ログインと新規登録のqueryを作成。

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,94 +1,28 @@
-import {
-  Box,
-  Button,
-  ChakraProps,
-  Container,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Stack,
-  VStack,
-} from '@chakra-ui/react'
-import React, { FC } from 'react'
-import { usePagesSigninIndexQuery } from './index.gen'
-import { PagePadding } from '@/components/layout/PagePadding'
-import { PageRoot } from '@/components/layout/PageRoot'
-import { Footer } from '@/components/navigation/Footer'
-import { Header } from '@/components/navigation/Header'
+import React from 'react'
+import { Signin } from './Signin'
+import { usePagesSigninMutation } from './index.gen'
 
-const SigninPage: FC<ChakraProps> = () => {
-  const { data, error, loading } = usePagesSigninIndexQuery()
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-
-  console.log(data)
+const SigninPage = () => {
+  const [signin] = usePagesSigninMutation({
+    onCompleted(data) {
+      console.log('Login successful:', data)
+    },
+    onError: (error) => console.log('Error:', error.message),
+  })
 
   return (
-    <PageRoot backgroundColor="gray.50">
-      <Header />
-      <Box>
-        <Container
-          bg="white"
-          boxShadow="md"
-          p={12}
-          mt="96px"
-          border="3px solid white"
-          borderColor="#850b0bf"
-        >
-          <VStack>
-            <Heading fontSize="lg">ログイン</Heading>
-          </VStack>
-          <form action="">
-            <Stack spacing={4} mt="64px">
-              <FormControl id="email">
-                <FormLabel fontWeight="600" color="gray.800">
-                  メールアドレス
-                </FormLabel>
-                <Input
-                  type="text"
-                  size="lg"
-                  inputMode="email"
-                  px={4}
-                  py={8}
-                  placeholder="メールアドレスを入力"
-                />
-              </FormControl>
-              <FormControl id="password" mt="6">
-                <FormLabel fontWeight="600" color="gray.800">
-                  パスワード
-                </FormLabel>
-                <Input
-                  type="password"
-                  size="lg"
-                  placeholder="パスワードを入力"
-                  px={4}
-                  py={8}
-                />
-              </FormControl>
-
-              <Stack spacing={10} mt={12}>
-                <Button
-                  bg="yellow.200"
-                  width="100%"
-                  size="lg"
-                  fontWeight="bold"
-                  type="submit"
-                  px={4}
-                  py={8}
-                  _hover={{ bg: 'yellow.300' }}
-                >
-                  ログイン
-                </Button>
-              </Stack>
-            </Stack>
-          </form>
-        </Container>
-      </Box>
-      <PagePadding />
-      <Footer />
-    </PageRoot>
+    <Signin
+      onSubmit={(values) => {
+        signin({
+          variables: {
+            input: {
+              email: values.email,
+              password: values.password,
+            },
+          },
+        })
+      }}
+    />
   )
 }
 
