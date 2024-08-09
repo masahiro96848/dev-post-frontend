969f3dd2460c2db15262b3df8e7ee395672fcd32
masahiro96848 <feetwcpic9436@gmail.com>
2024-08-10 00:39:51 +0900

    記事投稿のスマホレイアウトを作成。

--- src/pages/posts/[postId]/edit/PostEdit.tsx
+++ src/pages/posts/[postId]/edit/PostEdit.tsx
@@ -13,7 +13,7 @@ import {
   Image,
   IconButton,
 } from '@chakra-ui/react'
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { FaRegImage } from 'react-icons/fa'
 import { PagePadding } from '@/components/layout/PagePadding'
@@ -30,6 +30,7 @@ type FormValues = {
 }
 
 export const PostEdit: FC = () => {
+  const [isMobile, setIsMobile] = useState(false)
   const [imageSrc, setImageSrc] = useState<string | null>(null)
   const [isPublished, setIsPublished] = useState(false)
   const {
@@ -65,24 +66,32 @@ export const PostEdit: FC = () => {
     setIsPublished((prev) => !prev)
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    handleResize() // 初期実行
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
     <PageRoot backgroundColor="gray.50">
       <Header />
-      <Box p={12}>
-        <Container
-          maxWidth="1200px"
-          bg="white"
-          boxShadow="md"
-          p={12}
-          mt="96px"
-          border="3px solid white"
-          borderColor="#850b0bf"
-        >
-          <form onSubmit={handleSubmit((v) => v)}>
-            <Flex>
+      <Box p={4}>
+        {isMobile ? (
+          <Container
+            bg="white"
+            boxShadow="md"
+            p={8}
+            border="3px solid white"
+            borderColor="#850b0bf"
+          >
+            <form onSubmit={handleSubmit((v) => v)}>
               <Stack spacing={4} flex="1">
                 <FormControl id="title" isInvalid={!!errors.title}>
-                  <FormLabel fontWeight="600" color="gray.800">
+                  <FormLabel fontSize="sm" fontWeight="600" color="gray.800">
                     記事タイトル
                   </FormLabel>
                   <Input
@@ -99,7 +108,7 @@ export const PostEdit: FC = () => {
                 </FormControl>
 
                 <FormControl id="description" mt="6">
-                  <FormLabel fontWeight="600" color="gray.800">
+                  <FormLabel fontSize="sm" fontWeight="600" color="gray.800">
                     記事概要
                   </FormLabel>
                   <Input
@@ -116,21 +125,23 @@ export const PostEdit: FC = () => {
                 </FormControl>
               </Stack>
 
-              <Box ml={8} flexShrink={0}>
-                <FormControl>
-                  <FormLabel fontWeight="600" color="gray.800">
+              <Box flexShrink={0}>
+                <FormControl mt="6">
+                  <FormLabel
+                    textAlign="left"
+                    fontSize="sm"
+                    fontWeight="600"
+                    color="gray.800"
+                  >
                     画像をアップロード
                   </FormLabel>
                   <Flex
                     align="center"
-                    justify="center"
+                    justify="left"
                     direction="column"
                     border="1px dashed gray"
-                    p={4}
                     borderRadius="md"
                     cursor="pointer"
-                    width="300px"
-                    height="200px"
                     onClick={triggerFileUpload}
                   >
                     {imageSrc ? (
@@ -150,7 +161,7 @@ export const PostEdit: FC = () => {
                           variant="ghost"
                           fontSize="3xl"
                         />
-                        <Button variant="ghost" mt={2}>
+                        <Button fontSize="sm" variant="ghost" mt={2}>
                           画像をアップロード
                         </Button>
                       </>
@@ -165,56 +176,203 @@ export const PostEdit: FC = () => {
                   </Flex>
                 </FormControl>
               </Box>
-            </Flex>
 
-            <FormControl id="body" mt="6">
-              <FormLabel fontWeight="600" color="gray.800">
-                本文
-              </FormLabel>
-              <Textarea
-                size="lg"
-                placeholder="技術内容を入力"
-                height={600}
-                {...register('body')}
-              />
-            </FormControl>
-
-            <Flex align="center" justify="space-between" mt={6}>
-              <FormControl display="flex" alignItems="center">
-                <FormLabel fontWeight="600" color="gray.800" mb="0">
-                  公開ステータス
+              <FormControl id="body" mt="6">
+                <FormLabel fontSize="sm" fontWeight="600" color="gray.800">
+                  本文
                 </FormLabel>
-                <Switch
-                  id="publish-status"
-                  ml={4}
+                <Textarea
                   size="lg"
-                  isChecked={isPublished}
-                  onChange={handleToggleChange}
+                  placeholder="技術内容を入力"
+                  height={400}
+                  {...register('body')}
                 />
               </FormControl>
-            </Flex>
 
-            <Stack spacing={10} mt={12}>
-              <Button
-                bg={isPublished ? 'black' : 'gray.300'}
-                color={isPublished ? 'white' : 'black'}
-                width="50%"
-                mx="auto"
-                size="lg"
-                fontWeight="bold"
-                type="submit"
-                px={4}
-                py={8}
-                _hover={{
-                  bg: isPublished ? 'black' : 'gray.400',
-                }}
-                isDisabled={!isValid}
-              >
-                {isPublished ? '記事を公開する' : '下書きを保存する'}
-              </Button>
-            </Stack>
-          </form>
-        </Container>
+              <Flex align="center" justify="space-between" mt={6}>
+                <FormControl display="flex" alignItems="center">
+                  <FormLabel fontWeight="600" color="gray.800" mb="0">
+                    公開ステータス
+                  </FormLabel>
+                  <Switch
+                    id="publish-status"
+                    ml={4}
+                    size="lg"
+                    isChecked={isPublished}
+                    onChange={handleToggleChange}
+                  />
+                </FormControl>
+              </Flex>
+
+              <Stack spacing={10} mt={12}>
+                <Button
+                  bg={isPublished ? 'black' : 'gray.300'}
+                  color={isPublished ? 'white' : 'black'}
+                  width="100%"
+                  mx="auto"
+                  size="lg"
+                  fontWeight="bold"
+                  type="submit"
+                  px={4}
+                  py={8}
+                  _hover={{
+                    bg: isPublished ? 'black' : 'gray.400',
+                  }}
+                  isDisabled={!isValid}
+                >
+                  {isPublished ? '記事を公開する' : '下書きを保存する'}
+                </Button>
+              </Stack>
+            </form>
+          </Container>
+        ) : (
+          <Container
+            maxWidth="1200px"
+            bg="white"
+            boxShadow="md"
+            p={12}
+            mt="48px"
+            border="3px solid white"
+            borderColor="#850b0bf"
+          >
+            <form onSubmit={handleSubmit((v) => v)}>
+              <Flex>
+                <Stack spacing={4} flex="1">
+                  <FormControl id="title" isInvalid={!!errors.title}>
+                    <FormLabel fontWeight="600" color="gray.800">
+                      記事タイトル
+                    </FormLabel>
+                    <Input
+                      type="text"
+                      size="lg"
+                      px={4}
+                      py={8}
+                      placeholder="タイトル"
+                      {...register('title')}
+                    />
+                    <FormErrorMessage>
+                      {errors.title && errors.title.message}
+                    </FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl id="description" mt="6">
+                    <FormLabel fontWeight="600" color="gray.800">
+                      記事概要
+                    </FormLabel>
+                    <Input
+                      type="text"
+                      size="lg"
+                      placeholder="概要を入力"
+                      px={4}
+                      py={8}
+                      {...register('description')}
+                    />
+                    <FormErrorMessage>
+                      {errors.description && errors.description.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </Stack>
+
+                <Box ml={8} flexShrink={0}>
+                  <FormControl>
+                    <FormLabel fontWeight="600" color="gray.800">
+                      画像をアップロード
+                    </FormLabel>
+                    <Flex
+                      align="center"
+                      justify="center"
+                      direction="column"
+                      border="1px dashed gray"
+                      p={4}
+                      borderRadius="md"
+                      cursor="pointer"
+                      width="300px"
+                      height="200px"
+                      onClick={triggerFileUpload}
+                    >
+                      {imageSrc ? (
+                        <Image
+                          src={imageSrc}
+                          alt="Uploaded image preview"
+                          width="300px"
+                          height="200px"
+                          objectFit="cover"
+                          borderRadius="md"
+                        />
+                      ) : (
+                        <>
+                          <IconButton
+                            icon={<FaRegImage />}
+                            aria-label="Upload Image"
+                            variant="ghost"
+                            fontSize="3xl"
+                          />
+                          <Button variant="ghost" mt={2}>
+                            画像をアップロード
+                          </Button>
+                        </>
+                      )}
+                      <Input
+                        id="image-upload"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        display="none"
+                      />
+                    </Flex>
+                  </FormControl>
+                </Box>
+              </Flex>
+
+              <FormControl id="body" mt="6">
+                <FormLabel fontWeight="600" color="gray.800">
+                  本文
+                </FormLabel>
+                <Textarea
+                  size="lg"
+                  placeholder="技術内容を入力"
+                  height={600}
+                  {...register('body')}
+                />
+              </FormControl>
+
+              <Flex align="center" justify="space-between" mt={6}>
+                <FormControl display="flex" alignItems="center">
+                  <FormLabel fontWeight="600" color="gray.800" mb="0">
+                    公開ステータス
+                  </FormLabel>
+                  <Switch
+                    id="publish-status"
+                    ml={4}
+                    size="lg"
+                    isChecked={isPublished}
+                    onChange={handleToggleChange}
+                  />
+                </FormControl>
+              </Flex>
+
+              <Stack spacing={10} mt={12}>
+                <Button
+                  bg={isPublished ? 'black' : 'gray.300'}
+                  color={isPublished ? 'white' : 'black'}
+                  width="50%"
+                  mx="auto"
+                  size="lg"
+                  fontWeight="bold"
+                  type="submit"
+                  px={4}
+                  py={8}
+                  _hover={{
+                    bg: isPublished ? 'black' : 'gray.400',
+                  }}
+                  isDisabled={!isValid}
+                >
+                  {isPublished ? '記事を公開する' : '下書きを保存する'}
+                </Button>
+              </Stack>
+            </form>
+          </Container>
+        )}
       </Box>
       <PagePadding />
       <Footer />
