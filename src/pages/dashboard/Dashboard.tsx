11eee6ba553d036d4c11f4d7fa198b9b2f3c0736
masahiro96848 <feetwcpic9436@gmail.com>
2024-09-02 00:40:52 +0900

    ダッシュボードにソート機能のgraphqlクエリを作成。ソートのロジックを作成。

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,171 +1,308 @@
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import {
   Box,
-  Container,
-  Heading,
   Flex,
-  Button,
+  Heading,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
+  Button,
+  Container,
 } from '@chakra-ui/react'
-import React, { FC, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { FC, useEffect, useState, useMemo, useCallback } from 'react'
 import { PagePadding } from '@/components/layout/PagePadding'
 import { PageRoot } from '@/components/layout/PageRoot'
 import { Footer } from '@/components/navigation/Footer'
 import { Header } from '@/components/navigation/Header'
 import { Sidebar } from '@/components/navigation/Sidebar'
+import { Pagination } from '@/components/ui/Pagination'
 import { MemoizedMyPostCard } from '@/components/ui/myPost/Card'
 import { Post, User } from '@/types/graphql.gen'
 
 type Props = {
   viewer: User
-  myPosts: Post[]
+  myposts: Post[]
+  currentPage: number
+  postsPerPage: number
+  setCurrentPage: (page: number) => void
+  setSortBy: (sortBy: string) => void
+  setOrder: (order: string) => void
 }
+
 export const Dashboard: FC<Props> = (props: Props) => {
-  const { viewer, myPosts } = props
-  const [isMobile, setIsMobile] = useState(false)
+  const {
+    viewer,
+    myposts,
+    currentPage,
+    postsPerPage,
+    setCurrentPage,
+    setSortBy,
+    setOrder,
+  } = props
+
+  const router = useRouter()
+  const [isMobile, setIsMobile] = useState<boolean>(false)
+  const [isTablet, setIsTablet] = useState<boolean>(false)
+  const [selectedSort, setSelectedSort] = useState<string>('作成日順')
+
+  const totalPages = useMemo(
+    () => Math.ceil(myposts.length / postsPerPage),
+    [myposts, postsPerPage],
+  )
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768)
+      const width = window.innerWidth
+      setIsMobile(width < 768)
+      setIsTablet(width >= 768 && width < 1024)
     }
 
-    handleResize() // 初期実行
+    handleResize()
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
+
+  useEffect(() => {
+    if (router.query.sort_by) {
+      setSortBy(router.query.sort_by as string)
+      setSelectedSort(
+        router.query.sort_by === 'is_published' && router.query.order === 'asc'
+          ? '公開済み'
+          : router.query.sort_by === 'is_published' &&
+              router.query.order === 'desc'
+            ? '保存中'
+            : '作成日順',
+      )
+    }
+    if (router.query.order) {
+      setOrder(router.query.order as string)
+    }
+    if (router.query.page) {
+      setCurrentPage(parseInt(router.query.page as string) || 1)
+    }
+  }, [
+    router.query.sort_by,
+    router.query.order,
+    router.query.page,
+    setSortBy,
+    setOrder,
+    setCurrentPage,
+  ])
+
+  const handlePageChange = useCallback(
+    (page: number) => {
+      setCurrentPage(page)
+      router.push(
+        {
+          pathname: router.pathname,
+          query: { ...router.query, page: String(page) },
+        },
+        undefined,
+        { shallow: true },
+      )
+    },
+    [setCurrentPage, router],
+  )
+
+  const handleSortChange = useCallback(
+    (sort_by: string, order: string, label: string) => {
+      setSortBy(sort_by)
+      setOrder(order)
+      setSelectedSort(label)
+      setCurrentPage(1)
+      router.push(
+        {
+          pathname: router.pathname,
+          query: { sort_by, order, page: '1' },
+        },
+        undefined,
+        { shallow: true },
+      )
+    },
+    [setSortBy, setOrder, setCurrentPage, router],
+  )
 
   return (
     <PageRoot backgroundColor="gray.50">
       <Header viewer={viewer} />
       <Box>
         {isMobile ? (
-          <Box>
-            <Tabs>
-              <TabList>
-                <Tab>投稿記事一覧</Tab>
-                <Tab>プロフィール変更</Tab>
-              </TabList>
-
-              <TabPanels>
-                <TabPanel>
-                  <Box py="24px">
-                    <Heading fontSize="2xl" textAlign="left">
-                      投稿記事一覧
-                    </Heading>
-                  </Box>
-                  <Box py="12px">
-                    <Flex justifyContent="flex-end" alignItems="center">
-                      <Menu>
-                        <MenuButton
-                          as={Button}
-                          rightIcon={<ChevronDownIcon />}
-                          backgroundColor="gray.500"
-                          color="white"
-                          _hover={{ backgroundColor: 'blue.600' }}
-                        >
-                          すべて
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem>すべて</MenuItem>
-                          <MenuItem>公開済み</MenuItem>
-                          <MenuItem>保存中</MenuItem>
-                        </MenuList>
-                      </Menu>
-                    </Flex>
-                  </Box>
-                  <Box>
-                    <Flex wrap="wrap" justifyContent="center">
-                      {myPosts.map((post, index) => (
-                        <MemoizedMyPostCard
-                          key={index}
-                          myPost={post}
-                          isMobile={isMobile}
-                        />
-                      ))}
-                    </Flex>
-                  </Box>
-                </TabPanel>
-                <TabPanel>
-                  <p>two!</p>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
+          <Box p="24px">
+            <Box py="24px">
+              <Heading fontSize="2xl" textAlign="left">
+                投稿記事一覧
+              </Heading>
+            </Box>
+            <Box py="12px">
+              <Flex justifyContent="flex-end" alignItems="center">
+                <Menu>
+                  <MenuButton
+                    as={Button}
+                    rightIcon={<ChevronDownIcon />}
+                    backgroundColor="gray.500"
+                    color="white"
+                    _hover={{ backgroundColor: 'blue.600' }}
+                  >
+                    {selectedSort}
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem
+                      onClick={() =>
+                        handleSortChange('created_at', 'desc', '作成日順')
+                      }
+                    >
+                      作成日順
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() =>
+                        handleSortChange('is_published', 'asc', '公開済み')
+                      }
+                    >
+                      公開済み
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() =>
+                        handleSortChange('is_published', 'desc', '保存中')
+                      }
+                    >
+                      保存中
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Flex>
+            </Box>
+            <Box>
+              <Flex wrap="wrap" justifyContent="center">
+                {myposts.map((mypost, index) => (
+                  <MemoizedMyPostCard
+                    key={index}
+                    myPost={mypost}
+                    isMobile={isMobile}
+                  />
+                ))}
+              </Flex>
+              {totalPages > 1 && (
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              )}
+            </Box>
           </Box>
         ) : (
-          <Flex justifyContent="center" mt="48px">
-            <Container maxWidth="1400px" bg="white" boxShadow="md" p={12}>
-              <Flex>
-                <Sidebar />
+          <Box p={isTablet ? '24px' : '96px'}>
+            <Flex justifyContent="center">
+              <Container maxWidth="1400px" bg="white" boxShadow="md" p={12}>
+                <Flex>
+                  <Sidebar />
 
-                <Box p="24px">
-                  <Box
-                    width="100%"
-                    maxWidth="1460px"
-                    mx="auto"
-                    px={{ base: '8px', md: '24px' }}
-                    py="24px"
-                  >
-                    <Heading
+                  <Box p="24px">
+                    <Box
+                      width="100%"
+                      maxWidth="1460px"
+                      mx="auto"
                       px={{ base: '8px', md: '24px' }}
-                      fontSize="2xl"
-                      textAlign="left"
+                      py="24px"
                     >
-                      投稿記事一覧
-                    </Heading>
-                  </Box>
-                  <Box maxWidth="1400px" mx="auto" px="96px" py="24px">
-                    <Flex justifyContent="flex-end" alignItems="center">
-                      <Menu>
-                        <MenuButton
-                          as={Button}
-                          rightIcon={<ChevronDownIcon />}
-                          backgroundColor="gray.500"
-                          color="white"
-                          _hover={{ backgroundColor: 'blue.600' }}
-                        >
-                          すべて
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem>すべて</MenuItem>
-                          <MenuItem>公開済み</MenuItem>
-                          <MenuItem>下書き</MenuItem>
-                        </MenuList>
-                      </Menu>
-                    </Flex>
-                  </Box>
-                  <Box
-                    display="flex"
-                    justifyContent="center"
-                    px={{ base: '8px', md: '24px' }}
-                  >
-                    <Box width="100%" maxWidth="1400px" px={{ base: '8px' }}>
-                      <Flex
-                        wrap="wrap"
-                        gap={{ base: '8px', md: '8px' }}
-                        justifyContent="flex-start"
+                      <Heading
+                        px={{ base: '8px', md: '24px' }}
+                        fontSize="2xl"
+                        textAlign="left"
                       >
-                        {myPosts.map((post, index) => (
-                          <MemoizedMyPostCard
-                            key={index}
-                            myPost={post}
-                            isMobile={isMobile}
-                          />
-                        ))}
+                        投稿記事一覧
+                      </Heading>
+                    </Box>
+                    <Box
+                      maxWidth="1400px"
+                      mx="auto"
+                      px={isTablet ? '12px' : '96px'}
+                      py="24px"
+                    >
+                      <Flex justifyContent="flex-end" alignItems="center">
+                        <Menu>
+                          <MenuButton
+                            as={Button}
+                            rightIcon={<ChevronDownIcon />}
+                            backgroundColor="gray.500"
+                            color="white"
+                            _hover={{ backgroundColor: 'blue.600' }}
+                          >
+                            {selectedSort}
+                          </MenuButton>
+                          <MenuList>
+                            <MenuItem
+                              onClick={() =>
+                                handleSortChange(
+                                  'created_at',
+                                  'desc',
+                                  '作成日順',
+                                )
+                              }
+                            >
+                              作成日順
+                            </MenuItem>
+                            <MenuItem
+                              onClick={() =>
+                                handleSortChange(
+                                  'is_published',
+                                  'asc',
+                                  '公開済み',
+                                )
+                              }
+                            >
+                              公開済み
+                            </MenuItem>
+                            <MenuItem
+                              onClick={() =>
+                                handleSortChange(
+                                  'is_published',
+                                  'desc',
+                                  '保存中',
+                                )
+                              }
+                            >
+                              保存中
+                            </MenuItem>
+                          </MenuList>
+                        </Menu>
                       </Flex>
                     </Box>
+                    <Box
+                      display="flex"
+                      justifyContent="center"
+                      px={{ base: '8px', md: '24px' }}
+                    >
+                      <Box
+                        width="100%"
+                        maxWidth="1400px"
+                        px={{ base: '8px', md: '24px' }}
+                      >
+                        <Flex
+                          wrap="wrap"
+                          gap={{ base: '8px', md: '8px' }}
+                          justifyContent="flex-start"
+                        >
+                          {myposts.map((mypost, index) => (
+                            <MemoizedMyPostCard key={index} myPost={mypost} />
+                          ))}
+                        </Flex>
+                        {totalPages > 1 && (
+                          <Pagination
+                            currentPage={currentPage}
+                            totalPages={totalPages}
+                            onPageChange={handlePageChange}
+                          />
+                        )}
+                      </Box>
+                    </Box>
                   </Box>
-                </Box>
-              </Flex>
-            </Container>
-          </Flex>
+                </Flex>
+              </Container>
+            </Flex>
+          </Box>
         )}
       </Box>
       <PagePadding />
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,25 +1,93 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import React, { useState, useMemo, useEffect } from 'react'
 import { Dashboard } from './Dashboard'
-import { usePagesDashboardIndexQuery } from './index.gen'
+import {
+  usePagesDashboardIndexQuery,
+  usePagesDashboardMypostsSortMutation,
+} from './index.gen'
 import { Loading } from '@/toastModal/Loading'
 import { useApolloErrorToast } from '@/toastModal/useApolloErrorToast'
+import { Post } from '@/types/graphql.gen'
 
 const Page: NextPage = () => {
   const router = useRouter()
-  const apolloErrorToast = useApolloErrorToast()
-  const { data } = usePagesDashboardIndexQuery({
-    fetchPolicy: 'cache-and-network',
-    onError(e) {
-      apolloErrorToast(e)
-      router.push('/signin')
-    },
-  })
+  const { query } = router
 
-  if (!data?.viewer) {
+  const apolloErrorToast = useApolloErrorToast()
+
+  const postsPerPage = 12
+
+  // クエリパラメータから状態を初期化
+  const [currentPage, setCurrentPage] = useState<number>(
+    parseInt(query.page as string) || 1,
+  )
+  const [sortBy, setSortBy] = useState<string>(
+    query.sort_by ? (query.sort_by as string) : 'created_at',
+  )
+  const [order, setOrder] = useState<string>(
+    query.order ? (query.order as string) : 'desc',
+  )
+  const [sortedPosts, setSortedPosts] = useState<Post[]>([])
+
+  const offset = useMemo(
+    () => (currentPage - 1) * postsPerPage,
+    [currentPage, postsPerPage],
+  )
+
+  const { data: indexData, loading: indexLoading } =
+    usePagesDashboardIndexQuery({
+      fetchPolicy: 'cache-and-network',
+      variables: {
+        limit: postsPerPage,
+        offset,
+      },
+      onError(e) {
+        apolloErrorToast(e)
+        router.push('/signin')
+      },
+    })
+
+  // ソート用ミューテーション
+  const [mypostSort, { loading: sortLoading }] =
+    usePagesDashboardMypostsSortMutation({
+      onCompleted: (data) => {
+        setSortedPosts(data.mypostSort.myposts || [])
+      },
+      onError: apolloErrorToast,
+    })
+
+  // クエリパラメータに基づいてソートとページングを実行
+  useEffect(() => {
+    mypostSort({
+      variables: {
+        sortBy,
+        order,
+        limit: postsPerPage,
+        offset,
+      },
+    })
+  }, [sortBy, order, currentPage, offset, mypostSort, postsPerPage])
+
+  if (indexLoading || sortLoading) {
     return <Loading />
   }
-  return <Dashboard viewer={data.viewer} myPosts={data.myPosts} />
+
+  if (!indexData) {
+    return <Loading />
+  }
+
+  return (
+    <Dashboard
+      viewer={indexData.viewer}
+      myposts={sortedPosts.length > 0 ? sortedPosts : indexData.myposts}
+      currentPage={currentPage}
+      postsPerPage={postsPerPage}
+      setCurrentPage={setCurrentPage}
+      setSortBy={setSortBy}
+      setOrder={setOrder}
+    />
+  )
 }
 
 export default Page
