ab919b4a041da216eb961706fef336e3396d2790
masahiro96848 <feetwcpic9436@gmail.com>
2024-10-20 22:14:30 +0900

    webServerを修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    port: 3000,
+    port: 3001,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
